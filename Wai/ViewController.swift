e7b459df0081aec44f8540749130a0f3498b1e99
tkhawari <taimurhaider111@hotmail.com>
2016-04-06 17:11:33 +1000

    first-commit :)

---
+++ Wai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wai
+//
+//  Created by TAIMUR KHAWARI on 6/04/2016.
+//  Copyright © 2016 wtt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
