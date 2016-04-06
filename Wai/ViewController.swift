d5f31c9ff58faaaee39a5ed54a56c54462dd5caf
Walid <wallidhajj@gmail.com>
2016-04-06 20:25:58 +1000

    add basic user validation

--- Wai/ViewController.swift
+++ Wai/ViewController.swift
@@ -9,8 +9,11 @@
 // Need to implement
 import UIKit
 
+var accounts : [Account] = [Account(id: 1,username: "wallid",password: "password",email: "email",type: AccountType.USER),Account(id: 2,username: "wallid1",password: "password",email: "email",type: AccountType.USER),Account(id: 3,username: "wallid2",password: "password",email: "email",type: AccountType.USER)]
+
 class ViewController: UIViewController {
 
+    var accountFound = false
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -24,10 +27,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    // Valdiate User Login
     @IBAction func signIn(sender: UIButton) {
-    
-    
+        let username = nameTextField.text
+        let password = passwordTextField.text
+        
+        for account in accounts{
+            if account.validateLogin(username!, password: password!){
+                accountFound = true
+            }
+        }
+        
     }
 
 }
