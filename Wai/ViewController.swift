da50bff85d1993e254cdca7ed6a80bf2057e7955
Walid <wallidhajj@gmail.com>
2016-04-06 19:26:28 +1000

    connect ui to view controller

--- Wai/ViewController.swift
+++ Wai/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signIn(sender: UIButton) {
+    
+    
+    }
 
 }
 
